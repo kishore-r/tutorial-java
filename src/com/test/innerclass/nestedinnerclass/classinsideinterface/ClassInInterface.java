70c21a7dd80f9581ad0fe01b64098784c47c269b
kishore-r <tokishore@hotmail.com>
2017-10-17 14:53:10 +0800

    Update ClassInInterface.java

--- src/com/test/innerclass/nestedinnerclass/classinsideinterface/ClassInInterface.java
+++ src/com/test/innerclass/nestedinnerclass/classinsideinterface/ClassInInterface.java
@@ -8,7 +8,7 @@
  *
  * Initial Creation:
  *    Author      rak
- *    Created on  Oct 17, 2017
+ *    Created on  Oct 17, 2015
  *
  ************************************************************************/
 package com.test.innerclass.nestedinnerclass.classinsideinterface;
@@ -17,15 +17,15 @@ package com.test.innerclass.nestedinnerclass.classinsideinterface;
  * ----------------------------------------------
  * WHAT?
  * ----------------------------------------------
- * Normally, you can’t put any code inside an interface, but a nested class can be part of an
+ * Normally, you canâ€™t put any code inside an interface, but a nested class can be part of an
  * interface. Any class you put inside an interface is automatically public and static. Since the
- * class is static, it doesn’t violate the rules for interfaces—the nested class is only placed inside
+ * class is static, it doesnâ€™t violate the rules for interfacesâ€”the nested class is only placed inside
  * the namespace of the interface. You can even implement the surrounding interface in the
  * inner class, like this: 
  * 
  * ------------------------------------------------
  * WHY ?
- * It’s convenient to nest a class inside an interface when you want to create some common code
+ * Itâ€™s convenient to nest a class inside an interface when you want to create some common code
  * to be used with all different implementations of that interface. 
  * ------------------------------------------------
  */
